af68e620c66aade187b87ce19778b130eb3b9743
Lujia Jin <jinlujia@pku.edu.cn>
2022-07-01 15:04:53 +0800

    KPN.py updated

--- KPN.py
+++ KPN.py
@@ -5,34 +5,40 @@ import torch.nn.functional as F
 from torchsummary import summary
 import torchvision.models as models
 
+
 # KPN基本网路单元
 class Basic(nn.Module):
-    def __init__(self, in_ch, out_ch, g=16, channel_att=False, spatial_att=False):
+    def __init__(self, in_ch, out_ch, g=16, channel_att=False,
+                 spatial_att=False):
         super(Basic, self).__init__()
         self.channel_att = channel_att
         self.spatial_att = spatial_att
         self.conv1 = nn.Sequential(
-                nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, stride=1, padding=1),
-                # nn.BatchNorm2d(out_ch),
-                nn.ReLU(),
-                nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, stride=1, padding=1),
-                # nn.BatchNorm2d(out_ch),
-                nn.ReLU(),
-                nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, stride=1, padding=1),
-                # nn.BatchNorm2d(out_ch),
-                nn.ReLU()
-            )
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3,
+                      stride=1, padding=1),
+            # nn.BatchNorm2d(out_ch),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3,
+                      stride=1, padding=1),
+            # nn.BatchNorm2d(out_ch),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3,
+                      stride=1, padding=1),
+            # nn.BatchNorm2d(out_ch),
+            nn.ReLU()
+        )
 
         if channel_att:
             self.att_c = nn.Sequential(
-                nn.Conv2d(2*out_ch, out_ch//g, 1, 1, 0),
+                nn.Conv2d(2 * out_ch, out_ch // g, 1, 1, 0),
                 nn.ReLU(),
-                nn.Conv2d(out_ch//g, out_ch, 1, 1, 0),
+                nn.Conv2d(out_ch // g, out_ch, 1, 1, 0),
                 nn.Sigmoid()
             )
         if spatial_att:
             self.att_s = nn.Sequential(
-                nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3),
+                nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7,
+                          stride=1, padding=3),
                 nn.Sigmoid()
             )
 
@@ -45,26 +51,33 @@ class Basic(nn.Module):
         fm = self.conv1(data)
         if self.channel_att:
             # fm_pool = F.adaptive_avg_pool2d(fm, (1, 1)) + F.adaptive_max_pool2d(fm, (1, 1))
-            fm_pool = torch.cat([F.adaptive_avg_pool2d(fm, (1, 1)), F.adaptive_max_pool2d(fm, (1, 1))], dim=1)
+            fm_pool = torch.cat([F.adaptive_avg_pool2d(fm, (1, 1)),
+                                 F.adaptive_max_pool2d(fm, (1, 1))], dim=1)
             att = self.att_c(fm_pool)
             fm = fm * att
         if self.spatial_att:
-            fm_pool = torch.cat([torch.mean(fm, dim=1, keepdim=True), torch.max(fm, dim=1, keepdim=True)[0]], dim=1)
+            fm_pool = torch.cat([torch.mean(fm, dim=1, keepdim=True),
+                                 torch.max(fm, dim=1, keepdim=True)[0]], dim=1)
             att = self.att_s(fm_pool)
             fm = fm * att
         return fm
 
 
 class KPN(nn.Module):
-    def __init__(self, color=True, burst_length=8, blind_est=False, kernel_size=[5], sep_conv=False,
-                 channel_att=False, spatial_att=False, upMode='bilinear', core_bias=False):
+    def __init__(self, color=True, burst_length=8, blind_est=False,
+                 kernel_size=[5], sep_conv=False,
+                 channel_att=False, spatial_att=False, upMode='bilinear',
+                 core_bias=False):
         super(KPN, self).__init__()
         self.upMode = upMode
         self.burst_length = burst_length
         self.core_bias = core_bias
         self.color_channel = 3 if color else 1
-        in_channel = (3 if color else 1) * (burst_length if blind_est else burst_length+1)
-        out_channel = (3 if color else 1) * (2 * sum(kernel_size) if sep_conv else np.sum(np.array(kernel_size) ** 2)) * burst_length
+        in_channel = (3 if color else 1) * (
+            burst_length if blind_est else burst_length + 1)
+        out_channel = (3 if color else 1) * (
+            2 * sum(kernel_size) if sep_conv else np.sum(
+                np.array(kernel_size) ** 2)) * burst_length
         if core_bias:
             out_channel += (3 if color else 1) * burst_length
         # 各个卷积层定义
@@ -75,9 +88,12 @@ class KPN(nn.Module):
         self.conv4 = Basic(256, 512, channel_att=False, spatial_att=False)
         self.conv5 = Basic(512, 512, channel_att=False, spatial_att=False)
         # 6~8层要先上采样再卷积
-        self.conv6 = Basic(512+512, 512, channel_att=channel_att, spatial_att=spatial_att)
-        self.conv7 = Basic(256+512, 256, channel_att=channel_att, spatial_att=spatial_att)
-        self.conv8 = Basic(256+128, out_channel, channel_att=channel_att, spatial_att=spatial_att)
+        self.conv6 = Basic(512 + 512, 512, channel_att=channel_att,
+                           spatial_att=spatial_att)
+        self.conv7 = Basic(256 + 512, 256, channel_att=channel_att,
+                           spatial_att=spatial_att)
+        self.conv8 = Basic(256 + 128, out_channel, channel_att=channel_att,
+                           spatial_att=spatial_att)
         self.outc = nn.Conv2d(out_channel, out_channel, 1, 1, 0)
 
         self.kernel_pred = KernelConv(kernel_size, sep_conv, self.core_bias)
@@ -93,6 +109,17 @@ class KPN(nn.Module):
             nn.init.xavier_normal_(m.weight.data)
             nn.init.constant_(m.bias.data, 0.0)
 
+    @staticmethod
+    def pad_before_cat(x1, x2):
+        """Prevent the failure of skip concatenation between the corresponding
+        layers of the encoder and decoder due to the automatic edge trimming of
+        nn.Conv2d when the input image size is odd."""
+        diffY = x1.size()[-2] - x2.size()[-2]
+        diffX = x1.size()[-1] - x2.size()[-1]
+        x2 = F.pad(x2, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        return x2
+
     # 前向传播函数
     def forward(self, data_with_est, data, white_level=1.0):
         """
@@ -107,9 +134,27 @@ class KPN(nn.Module):
         conv4 = self.conv4(F.avg_pool2d(conv3, kernel_size=2, stride=2))
         conv5 = self.conv5(F.avg_pool2d(conv4, kernel_size=2, stride=2))
         # 开始上采样  同时要进行skip connection
-        conv6 = self.conv6(torch.cat([conv4, F.interpolate(conv5, scale_factor=2, mode=self.upMode)], dim=1))
-        conv7 = self.conv7(torch.cat([conv3, F.interpolate(conv6, scale_factor=2, mode=self.upMode)], dim=1))
-        conv8 = self.conv8(torch.cat([conv2, F.interpolate(conv7, scale_factor=2, mode=self.upMode)], dim=1))
+        # Padding before skip concatenation is needed, only works if the input
+        # image size is odd.
+        conv6 = self.conv6(torch.cat([conv4, self.pad_before_cat(conv4,
+                                                                 F.interpolate(
+                                                                     conv5,
+                                                                     scale_factor=2,
+                                                                     mode=self.upMode))],
+                                     dim=1))
+        conv7 = self.conv7(torch.cat([conv3, self.pad_before_cat(conv3,
+                                                                 F.interpolate(
+                                                                     conv6,
+                                                                     scale_factor=2,
+                                                                     mode=self.upMode))],
+                                     dim=1))
+        conv8 = self.conv8(torch.cat([conv2, self.pad_before_cat(conv2,
+                                                                 F.interpolate(
+                                                                     conv7,
+                                                                     scale_factor=2,
+                                                                     mode=self.upMode))],
+                                     dim=1))
+
         # return channel K*K*N
         core = self.outc(F.interpolate(conv8, scale_factor=2, mode=self.upMode))
 
@@ -120,6 +165,7 @@ class KernelConv(nn.Module):
     """
     the class of computing prediction
     """
+
     def __init__(self, kernel_size=[5], sep_conv=False, core_bias=False):
         super(KernelConv, self).__init__()
         self.kernel_size = sorted(kernel_size)
@@ -143,9 +189,13 @@ class KernelConv(nn.Module):
         core_out = {}
         cur = 0
         for K in self.kernel_size:
-            t1 = core_1[:, :, cur:cur + K, ...].view(batch_size, N, K, 1, 3, height, width)
-            t2 = core_2[:, :, cur:cur + K, ...].view(batch_size, N, 1, K, 3, height, width)
-            core_out[K] = torch.einsum('ijklno,ijlmno->ijkmno', [t1, t2]).view(batch_size, N, K * K, color, height, width)
+            t1 = core_1[:, :, cur:cur + K, ...].view(batch_size, N, K, 1, color,
+                                                     height, width)
+            t2 = core_2[:, :, cur:cur + K, ...].view(batch_size, N, 1, K, color,
+                                                     height, width)
+            core_out[K] = torch.einsum('ijklnou,ijlmnou->ijkmnou',
+                                       [t1, t2]).view(
+                batch_size, N, K * K, color, height, width)
             cur += K
         # it is a dict
         return core_out, None if not self.core_bias else core_3.squeeze()
@@ -158,7 +208,8 @@ class KernelConv(nn.Module):
         """
         core_out = {}
         core = core.view(batch_size, N, -1, color, height, width)
-        core_out[self.kernel_size[0]] = core[:, :, 0:self.kernel_size[0]**2, ...]
+        core_out[self.kernel_size[0]] = core[:, :, 0:self.kernel_size[0] ** 2,
+                                        ...]
         bias = None if not self.core_bias else core[:, :, -1, ...]
         return core_out, bias
 
@@ -176,29 +227,38 @@ class KernelConv(nn.Module):
             color = 1
             frames = frames.view(batch_size, N, color, height, width)
         if self.sep_conv:
-            core, bias = self._sep_conv_core(core, batch_size, N, color, height, width)
+            core, bias = self._sep_conv_core(core, batch_size, N, color, height,
+                                             width)
         else:
-            core, bias = self._convert_dict(core, batch_size, N, color, height, width)
+            core, bias = self._convert_dict(core, batch_size, N, color, height,
+                                            width)
         img_stack = []
         pred_img = []
         kernel = self.kernel_size[::-1]
         for index, K in enumerate(kernel):
-            if not img_stack:
+            if len(img_stack) == 0:
                 frame_pad = F.pad(frames, [K // 2, K // 2, K // 2, K // 2])
                 for i in range(K):
                     for j in range(K):
-                        img_stack.append(frame_pad[..., i:i + height, j:j + width])
+                        img_stack.append(
+                            frame_pad[..., i:i + height, j:j + width])
                 img_stack = torch.stack(img_stack, dim=2)
             else:
                 k_diff = (kernel[index - 1] - kernel[index]) // 2
-                img_stack = img_stack[:, :, k_diff:-k_diff, ...]
+                img_stack = img_stack.view(batch_size, N, kernel[index - 1],
+                                           kernel[index - 1], color, height,
+                                           width)
+                img_stack = img_stack[:, :, k_diff:-k_diff, k_diff:-k_diff, ...]
+                img_stack = img_stack.reshape(batch_size, N, K ** 2, color,
+                                              height, width)
             # print('img_stack:', img_stack.size())
             pred_img.append(torch.sum(
                 core[K].mul(img_stack), dim=2, keepdim=False
             ))
         pred_img = torch.stack(pred_img, dim=0)
         # print('pred_stack:', pred_img.size())
-        pred_img_i = torch.mean(pred_img, dim=0, keepdim=False).squeeze()
+        # pred_img_i = torch.mean(pred_img, dim=0, keepdim=False).squeeze()
+        pred_img_i = torch.mean(pred_img, dim=0, keepdim=False)
         # if bias is permitted
         if self.core_bias:
             if bias is None:
@@ -216,7 +276,9 @@ class LossFunc(nn.Module):
     """
     loss function of KPN
     """
-    def __init__(self, coeff_basic=1.0, coeff_anneal=1.0, gradient_L1=True, alpha=0.9998, beta=100):
+
+    def __init__(self, coeff_basic=1.0, coeff_anneal=1.0, gradient_L1=True,
+                 alpha=0.9998, beta=100):
         super(LossFunc, self).__init__()
         self.coeff_basic = coeff_basic
         self.coeff_anneal = coeff_anneal
@@ -232,13 +294,16 @@ class LossFunc(nn.Module):
         :param global_step: int
         :return: loss
         """
-        return self.coeff_basic * self.loss_basic(pred_img, ground_truth), self.coeff_anneal * self.loss_anneal(global_step, pred_img_i, ground_truth)
+        return self.coeff_basic * self.loss_basic(pred_img,
+                                                  ground_truth), self.coeff_anneal * self.loss_anneal(
+            global_step, pred_img_i, ground_truth)
 
 
 class LossBasic(nn.Module):
     """
     Basic loss function.
     """
+
     def __init__(self, gradient_L1=True):
         super(LossBasic, self).__init__()
         self.l1_loss = nn.L1Loss()
@@ -254,6 +319,7 @@ class LossAnneal(nn.Module):
     """
     anneal loss function
     """
+
     def __init__(self, alpha=0.9998, beta=100):
         super(LossAnneal, self).__init__()
         self.global_step = 0
@@ -279,6 +345,7 @@ class TensorGradient(nn.Module):
     """
     the gradient of tensor
     """
+
     def __init__(self, L1=True):
         super(TensorGradient, self).__init__()
         self.L1 = L1
@@ -290,13 +357,15 @@ class TensorGradient(nn.Module):
         u = F.pad(img, [0, 0, 1, 0])
         d = F.pad(img, [0, 0, 0, 1])
         if self.L1:
-            return torch.abs((l - r)[..., 0:w, 0:h]) + torch.abs((u - d)[..., 0:w, 0:h])
+            return torch.abs((l - r)[..., 0:w, 0:h]) + torch.abs(
+                (u - d)[..., 0:w, 0:h])
         else:
             return torch.sqrt(
-                torch.pow((l - r)[..., 0:w, 0:h], 2) + torch.pow((u - d)[..., 0:w, 0:h], 2)
+                torch.pow((l - r)[..., 0:w, 0:h], 2) + torch.pow(
+                    (u - d)[..., 0:w, 0:h], 2)
             )
 
 
 if __name__ == '__main__':
-    kpn = KPN(6, 5*5*6, True, True).cuda()
+    kpn = KPN(6, 5 * 5 * 6, True, True).cuda()
     print(summary(kpn, (6, 224, 224), batch_size=4))
